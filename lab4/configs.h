4cd2df739726315ac04ca0cdf6118a4ccb9cf82f
Vasco <vasco.dias.silva@tecnico.ulisboa.pt>
2019-12-02 17:15:50 +0000

    Change board layout

--- lab4/configs.h
+++ lab4/configs.h
@@ -6,20 +6,20 @@
 #define UNIT 1000 /* T=1second in specification */
 #define PERIOD 20 /* Total units of a full cycle */
 
-#define S_RED 4    /* Pin for red led in south direction */
-#define S_YEL 3    /* Pin for yellow led in south direction */
-#define S_GRE 2    /* Pin for green led in south direction */
-#define S_BUTTON 9 /* Pin for button (car counter) in south direction */
+#define S_RED 9    /* Pin for red led in south direction */
+#define S_YEL 8    /* Pin for yellow led in south direction */
+#define S_GRE 7    /* Pin for green led in south direction */
+#define S_BUTTON 6 /* Pin for button (car counter) in south direction */
 
-#define W_RED 7    /* Pin for red led in north direction */
-#define W_YEL 6    /* Pin for yellow led in north direction */
-#define W_GRE 5    /* Pin for green led in north direction */
-#define W_BUTTON 8 /* Pin for button (car counter) in north direction */
+#define W_RED 11   /* Pin for red led in north direction */
+#define W_YEL 12   /* Pin for yellow led in north direction */
+#define W_GRE 13   /* Pin for green led in north direction */
+#define W_BUTTON 10 /* Pin for button (car counter) in north direction */
 
 /* Bits for the Y coordinate */
-#define b0 12
-#define b1 11
-#define b2 10
+#define b0 5
+#define b1 4
+#define b2 3
 
 /* Bits for the X coordinate */
 #define a0 A0
@@ -28,7 +28,7 @@
 #define DETECT_PIN_W A2
 
 /* Bits for the cardinal points */
-#define DS 13
+#define DS 2
 #define DW A3
 
 #endif
